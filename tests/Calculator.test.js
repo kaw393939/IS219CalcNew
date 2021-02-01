98be10e5f70af2b6e1ad238f17ad473e4e2bcd51
keith <keith@webizly.com>
2021-02-01 14:39:56 -0500

    Test Merge Code

--- tests/Calculator.test.js
+++ tests/Calculator.test.js
@@ -3,6 +3,7 @@ const Calculator = require('../src/Calculator');
 test('Calculator adding two numbers', () => {
     //assigning the results of the calc sum method to an object, calculator returns an object
     let calculation = Calculator.Sum(1,2);
+    //expect is used to test if the results are correct
     expect(calculation.a).toBe(1);
     expect(calculation.b).toBe(2);
     expect(calculation.op).toBe("Sum");
